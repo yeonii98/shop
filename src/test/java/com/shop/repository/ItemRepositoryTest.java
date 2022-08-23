ad82d7e180006603e9139a2b344bb7044f106cc0
yeonii98 <ajy7424@naver.com>
2022-08-23 16:32:32 +0900

    QueryDSL Test

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,17 +1,24 @@
 package com.shop.repository;
 
+import com.querydsl.core.QueryResults;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static com.shop.entity.QItem.item;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -20,6 +27,9 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @PersistenceContext  //EntityManager 빈을 주입한다.
+    EntityManager em;
 
     @Test
     @DisplayName("상품 저장 테스트")
@@ -117,4 +127,38 @@ class ItemRepositoryTest {
         }
     }
 
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest(){
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em); //쿼리를 동적으로 생성한다. 생성자의 파라미터로는 EntityManager 객체를 넣어준다.
+        QItem qitem = item;
+        JPAQuery<Item> query = queryFactory.selectFrom(qitem)
+                .where(qitem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qitem.itemDetail.like("%"+ "상품 상세 설명" + "%"))
+                .orderBy(qitem.price.desc());
+
+        List<Item> itemList = query.fetch();  //쿼리를 리스트로 반환한다.
+
+        for(Item item : itemList){
+            System.out.println(item);
+        }
+    }
+
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트2")
+    public void queryDslTest2(){
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em); //쿼리를 동적으로 생성한다. 생성자의 파라미터로는 EntityManager 객체를 넣어준다.
+        QItem qitem = item;
+        JPAQuery<Item> query = queryFactory.selectFrom(qitem)
+                .where(qitem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qitem.itemDetail.like("%"+ "2" + "%"));
+
+        Item item = query.fetchOne();  //쿼리를 리스트로 반환한다.
+
+        System.out.println(item);
+
+    }
 }
