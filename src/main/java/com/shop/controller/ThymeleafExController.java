a509daa428d8b146670db90df897d479b050ccb3
yeonii98 <ajy7424@naver.com>
2022-08-28 16:49:49 +0900

    Thymeleaf example

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -4,11 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping(value = "/thymeleaf")
 public class ThymeleafExController {
-
     @GetMapping(value = "/ex01")
-    public String thymeleafExample01(Model model){
-        model.addAttribute("data","타임리프 예제 입니다.");
+    public String thymeleafExample01(Model model) {
+        model.addAttribute("data", "타임리프 예제 입니다.");
+        return "thymeleafEx/thymeleafEx01";
+    }
+}
